bb3ccdee295780e7074011a5f7786b0a7998b587
Javier Diaz <coderdiaz@gmail.com>
2018-08-15 02:10:08 -0500

    chore: packages configuration

---
+++ vue.config.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+
+// Getting entries for webpack with FileSystem
+const getEntries = () => {
+  return fs.readdirSync('./src')
+    .filter((file) => file.match(/.*\.js$/))
+    .map((file) => {
+      return {
+        name: file.substring(0, file.length - 3),
+        path: `./src/${file}`
+      }
+    }).reduce((memo, file) => {
+      memo[file.name] = file.path
+      return memo
+    }, {})
+}
+
+// Vue CLI config
+module.exports = {
+  outputDir: 'assets',
+  filenameHashing: false,
+  configureWebpack: config => {
+    // Deleting default entry
+    delete config.entry.app
+
+    // Added new entries based on src files
+    config.entry = getEntries()
+  },
+  chainWebpack: config => {
+    // delete HTML related webpack plugins
+    config.plugins.delete('html')
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
+
+    // Removing hash to img assets
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .tap(options => Object.assign({}, options, { name: 'img/[name].[ext]' }))
+  }
+}
