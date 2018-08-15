188373c436d9e6c52bc5e2c1bb121d3abe4d8cae
Javier Diaz <coderdiaz@gmail.com>
2018-08-15 02:13:37 -0500

    added article and home script

---
+++ src/article.js
@@ -0,0 +1,16 @@
+/**
+ * article.js
+ * ===============================================
+ * Article Page
+ * Javascript logic
+ */
+import Vue from 'vue'
+Vue.config.productionTip = false
+
+// eslint-disable-next-line
+new Vue({
+  el: '#app',
+  data () {
+    return {}
+  }
+})
---
+++ src/home.js
@@ -0,0 +1,16 @@
+/**
+ * home.js
+ * ===============================================
+ * Home Page
+ * Javascript logic
+ */
+import Vue from 'vue'
+Vue.config.productionTip = false
+
+// eslint-disable-next-line
+new Vue({
+  el: '#app',
+  data () {
+    return {}
+  }
+})
