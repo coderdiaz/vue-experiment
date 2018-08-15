b85eff3bf2d2812967c0120c2508450fc55260ed
Javier Diaz <coderdiaz@gmail.com>
2018-08-15 02:44:53 -0500

    fix: fixed jest alias

--- jest.config.js
+++ jest.config.js
@@ -11,12 +11,13 @@ module.exports = {
     '^.+\\.jsx?$': 'babel-jest'
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/packages/$1'
   },
   snapshotSerializers: [
     'jest-serializer-vue'
   ],
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ]
+  ],
+  testURL: 'http://localhost/'
 }
